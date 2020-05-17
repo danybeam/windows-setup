5dd42d176c51aabf726cd4cdd9c3b2faa801b3fb
Danybeam <danybeam@gmail.com>
2020-05-17 15:11:57 -0700

    release 1.0.0

---
+++ Config/Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+# Oh My PoSH config
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Agnoster # Personal favorite theme but more are available, check the ohmyposh repo for more :)
+
+# ALL THIS ROOM FOR ACTIVITIES
+
+# Aliases
