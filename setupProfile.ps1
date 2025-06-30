069f024d5785e0d8a549e6b0571350e5de5ec749
Daniel Orozco <danybeam@gmail.com>
2025-06-30 10:19:36 -0400

    Update setupProfile.ps1

--- setupProfile.ps1
+++ setupProfile.ps1
@@ -15,11 +15,42 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/danybeam.omp.json"| Invoke-
 oh-my-posh config migrate glyphs --write
 Import-Module -Name Terminal-Icons
 
-# Predictive text
-Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -EditMode Windows
-
 # better cd'ing
 Import-Module z
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Predictive text
+#Import-Module PSReadLine
+
+# Autocompleteion for Arrow keys
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -CompletionQueryItems 65
+
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Invoke-Expression (& { (jj util completion power-shell | Out-String) })
+
+function Update-Powershell {
+  param ()
+  iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+}
